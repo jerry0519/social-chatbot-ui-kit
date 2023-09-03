0153b03cae044af90ffa87cbb625aff0baa40401
jerry0519 <106452978+jerry0519@users.noreply.github.com>
2023-09-03 06:56:25 +0000

    const fix

--- src/chat/ChatMessage.jsx
+++ src/chat/ChatMessage.jsx
@@ -31,4 +31,56 @@ export function MessageHeader() {
           <Icon className={styles.icon} type="more" />
         </Popover>
         <Icon type="download" className={styles.icon} />
-      
+      </div>
+    </div>
+  )
+}
+
+export function EditorMessage() {
+  return (
+    <div>
+      <Textarea rows="3" />
+    </div>)
+}
+
+export function MessageItem(props) {
+  const { content, sentTime, role, } = props
+  const { removeMessage } = useGlobal()
+  return (
+    <div className={classnames(styles.item, styles[role])}>
+      <Avatar src={role !== 'user' && avatar} />
+      <div className={classnames(styles.item_content, styles[`item_${role}`])}>
+        <div className={styles.item_inner}>
+          <div className={styles.item_tool}>
+            <div className={styles.item_date}>{dateFormat(sentTime)}</div>
+            <div className={styles.item_bar}>
+              <Tooltip text="Remove Messages">
+                <Icon className={styles.icon} type="trash" onClick={removeMessage} />
+              </Tooltip>
+              {role === 'user' ? <React.Fragment>
+                <Icon className={styles.icon} type="reload" />
+                <Icon className={styles.icon} type="editor" />
+              </React.Fragment> : <CopyIcon value={content} />}
+            </div>
+          </div>
+          <MessageRender>
+            {content}
+          </MessageRender>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function MessageBar() {
+  const { sendMessage, setMessage, is, options, setIs, typeingMessage, clearTypeing, stopResonse } = useGlobal()
+  useSendKey(sendMessage, options.general.command)
+  return (
+    <div className={styles.bar}>
+      {is.thinking && <div className={styles.bar_tool}>
+        <div className={styles.bar_loading}>
+          <div className="flex-c"><span>Thinking</span> <Loading /></div><Button size="min" className={styles.stop} onClick={stopResonse} icon="stop">Stop Resonse</Button>
+        </div>
+      </div>}
+      <div className={styles.bar_inner}>
+        <div className={s
