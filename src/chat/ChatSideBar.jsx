cef5d155767844ccc1c2528cbca5f163903eb963
jerry0519 <106452978+jerry0519@users.noreply.github.com>
2023-09-06 12:50:11 +0000

    Fixing some warnings

--- src/chat/ChatSideBar.jsx
+++ src/chat/ChatSideBar.jsx
@@ -4,3 +4,21 @@ import { Avatar, Icon } from '../components'
 import { useGlobal } from './context'
 import styles from './style/sider.module'
 import { classnames } from '../components/utils'
+import { useOptions } from './hooks'
+
+export function ChatSideBar() {
+  const { is, setState, options } = useGlobal()
+  const { setGeneral } = useOptions()
+  return (
+    <div className={classnames(styles.sider, 'flex-c-sb flex-column')}>
+      <Avatar />
+      <div className={classnames(styles.tool, 'flex-c-sb flex-column')}>
+        <Icon className={styles.icon} type="apps" onClick={() => setState({ is: { ...is, apps: true } })} />
+        <Icon className={styles.icon} type="history" onClick={() => setState({ is: { ...is, apps: false } })} />
+        <Icon className={styles.icon} type={options.general.theme} onClick={() => setGeneral({ theme: options.general.theme === 'light' ? 'dark' : 'light' })} />
+        <Icon className={styles.icon} type="config" onClick={() => setState({ is: { ...is, config: !is.config } })} />
+        <Icon className={styles.icon} type={`${is.fullScreen ? 'min' : 'full'}-screen`} onClick={() => setState({ is: { ...is, fullScreen: !is.fullScreen } })} />
+      </div>
+    </div>
+  )
+}
---
+++ src/chat/MessageRender.jsx
@@ -0,0 +1,33 @@
+import React, { memo } from 'react'
+import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneLight, oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { useGlobal } from './context'
+import remarkMath from 'remark-math'
+import remarkGfm from 'remark-gfm'
+import remarkBreaks from 'remark-breaks'
+import './style/markdown.less'
+
+export const MessageRender = memo((props) => {
+  const { options } = useGlobal()
+  const style = options.general.theme === 'dark' ? oneDark : oneLight
+  return (
+    <ReactMarkdown
+      className="z-ui-markdown"
+      children={props.children}
+      remarkPlugins={[remarkMath, remarkGfm, remarkBreaks]}
+      components={{
+        code({ node, inline, className, children, ...rest }) {
+          const match = /language-(\w+)/.exec(className || '')
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...rest}
+              children={children}
+              style={style}
+              language={match[1]}
+              PreTag="div"
+            />
+          ) : (
+            <code {...props} className={`code-line`}>
+              {children}
+           
