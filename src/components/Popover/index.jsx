55ffe767d616c2c84a4044d372d7e8acf5ceb01f
jerry0519 <106452978+jerry0519@users.noreply.github.com>
2023-09-28 16:44:17 +0000

    l10n: Update

---
+++ src/components/Popover/index.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import styles from './popover.module.less';
+import { classnames } from '../utils';
+import { usePosition, useClickOutside } from '../hooks'
+
+export function Popover(props) {
+  const { content, position, trigger } = props;
+  const containerRef = useRef(null);
+  const triggerRef = useRef(null);
+  const boxRef = useRef(null);
+  const [visible, setVisible] = useClickOutside(boxRef, false)
+  const [contentPosition, setContenttPosition] = useState({})
+  const { x, y, width, height } = usePosition(triggerRef)
+  const { width: containerWidth, height: containerHeight } = usePosition(containerRef)
+
+  useEffect(() => {
+    if (x && y && width) {
+      setContenttPosition({ left: x + width, top: y, })
+    }
+  }, [x, y, width])
+
+  const togglePanel = () => {
+    setVisible(!visible)
+  }
+  const handleMouseEnter = () => {
+    trigger === 'hover' && togglePanel(true)
+  }
+  const handleMouseLeave = () => {
+    trigger === 'hover' && setVisible(false)
+  }
+  const handleTrigger = () => {
+    trigger
