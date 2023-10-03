a4024a25f166de55a296da479c47b5c2d73094a3
jerry0519 <106452978+jerry0519@users.noreply.github.com>
2023-10-03 11:05:11 +0000

    Version bump to 0.2.12

---
+++ src/components/Tabs/index.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import styles from './tabs.module.less'
+import { classnames } from '../utils';
+
+function Panel({ children }) {
+  return <div className={styles.panel}>{children}</div>
+}
+
+function Tabs({ children, tabsChange }) {
+  const [active, setActive] = useState(0);
+  {
+    React.Children.forEach(children, (child) => {
+      if (child.type?.name && child.type?.name !== 'Panel') {
+        console.warn('Invalid child component:', child.type);
+        return false
+      }
+    });
+  }
+  const handleTabsChange = (e, index) => {
+    setActive(index)
+    tabsChange && tabsChange(index, e)
+  }
+  const tabs = React.Children.toArray(children).filter(child => child.type === Panel);
+  return (
+    <div className={styles.tabs}>
+      <div className={styles.headers}>
+        {tabs.map((child, index) => (
+          <div
+         
