a9e3cf4b6abf70bca79909429915f61b65e48f69
jerry0519 <106452978+jerry0519@users.noreply.github.com>
2023-10-06 11:41:55 +0000

    Setting version to 1.0

---
+++ src/components/Textarea/index.jsx
@@ -0,0 +1,68 @@
+import React, { forwardRef, useState } from 'react'
+import { classnames } from '../utils';
+import Proptypes from 'prop-types'
+import { Button } from '../Button';
+import styles from './textarea.module.less'
+
+export const Textarea = forwardRef((props, ref) => {
+  const {
+    onChange,
+    placeholder,
+    className,
+    showClear,
+    disable,
+    children,
+    rows,
+    maxHeight,
+    value,
+    defaultValue,
+    transparent,
+    onClear,
+    ...rest
+  } = props;
+  const [content, setContent] = useState(value);
+  const [height, setHeight] = useState('auto')
+
+  function handleChange(event) {
+    setHeight('auto');
+    setHeight(`${event.target.scrollHeight}px`);
+    setContent(event.target.value);
+    onChange && onChange(event.target.value);
+  }
+
+  function handleClear() {
+    onChange && onChange("");
+    onClear && onClear();
+  }
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+    if (event.shiftKey && event.key === "Enter") {
+      setContent(content + "\n");
+      event.preventDefault();
+    }
+  }
+
+  return (
+    <div className={classnames(styles.textarea_box, className)}>
+      <div className={styles.inner}>
+        <textarea
+          ref={ref}
+          rows={rows}
+          style={{ height }}
+          onChange={handleChange}
+          placeholder={placeholder}
+          onKeyDown={handleKeyPress}
+          className={classnames(styles.textarea, transparent && styles.transparent)}
+          value={content}
+          {...rest}
+        />
+      </div>
+      {showClear && <Button className={styles.clear} type="icon" onClick={handleClear} icon="cancel" />}
+    </div>
+  );
+});
+
+Textarea.
