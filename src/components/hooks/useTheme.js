734e2629cd74327c1632dc061606a5fab6fb05b7
jerry0519 <106452978+jerry0519@users.noreply.github.com>
2023-10-11 19:37:31 +0000

    remove makefile

---
+++ src/components/hooks/useTheme.js
@@ -0,0 +1,24 @@
+
+import { useState, useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export function useTheme(initTheme) {
+  const [themes, setThemes] = useState("light");
+  const [theme, setTheme] = useLocalStorage("THEME", "light");
+  function toggleTheme() {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  }
+
+  useEffect(() => {
+    const body = document.querySelector("html");
+    body.classList.remove("light", "dark");
+    body.setAttribute("data-theme", theme);
+    body.classList.add(theme);
+  }, [theme]);
+
+  useEffect(() => {
+    setThemes(theme);
+  }, [theme]);
+
+  return [themes, toggleTheme];
+}
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import "@/assets/style/common.less";
+import "@/assets/icon/style.css";
+
+export { Button } from "./Button";
+export { Empty } from "./Empty";
+export { Icon } from "./Icon";
+export { Input } from "./Input";
+export { Logo } from "./Logo";
+export { Modal } from "./Modal";
+export { Paginati
