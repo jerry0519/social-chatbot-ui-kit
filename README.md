0565e4be2b5382ea50699e3e6db63cbffcd7a195
jerry0519 <106452978+jerry0519@users.noreply.github.com>
2023-10-15 20:29:23 +0000

    update: auto update by markdown generator

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# Social Chatbot User Interface Kit\n\nSocial-Chatbot-Ui-Kit is a simplistic, yet feature-packed React-based UI kit for chat-GPT. Useful features include chat streaming, code highlighting, code execution, and prebuilt development presets. We are urging you to contribute and aid in the improvement of this project!\n\n- Demo: [https://chat-gpt-ui-lake.vercel.app/](https://chat-gpt-ui-lake.vercel.app/)\n- Design source: [https://www.figma.com/community/file/1236510244313601424](https://www.figma.com/community/file/1236510244313601424)\n\n## Development\n\nClone the repository, install the necessary dependencies, and start the development server by following these steps:\n\n```
+  git clone https://github.com/jerry0519/social-chatbot-ui-kit.git
+  cd social-chatbot-ui-kit
+  npm install
+  npm run dev
+```
+\n## How to Contribute\n\nThe source code is straightforward and user-friendly. We welcome and appreciate any contributions to the project! Feel free to fork the repository, make your changes, and submit a pull request. If you have any questions or require assistance, don't hesitate to reach out. This project is licensed under the MIT License.\n\n## License\n\nThe user interface framework is open-source and bound by the MIT license. Refer to the LICENSE file for additional details.
--- webpack.config.js
+++ webpack.config.js
@@ -40,4 +40,82 @@ module.exports = {
         use: [
           stylesHandler,
           {
-  
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentHashFunction: "md4",
+                localIdentName: "[local]-[hash:5]",
+                // [name]_[local]_
+              },
+              importLoaders: 4,
+            },
+          },
+          "postcss-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: [stylesHandler, "css-loader", "postcss-loader"],
+      },
+
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: "single",
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          // parallel: true,
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+            unused: true,
+            dead_code: true,
+            reduce_vars: true,
+            // comments: false,
+            // extractComments: true,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
+    new HtmlWebpackPlugin({
+      title: "React Extension",
+      template: "./src/index.html",
+      filename: `index.html`,
+    }),
+    new MiniCssExtractPlugin({
+      filename: "static/css/[name].css",
+      chunkFilename: "static/css/[name].chunk.css",
+    }),
+  ],
+};
